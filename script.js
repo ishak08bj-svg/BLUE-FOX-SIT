2249d6c23192c1918a4e859ec60e3be7d1da8bf3
ishak08bj-svg <ishak08bj@gmail.com>
2026-01-29 08:19:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const btn = document.getElementById('themeBtn');
 btn.addEventListener('click', () => {
   document.body.classList.toggle('light');
 });
+<script>
+document.body.style.overflow = "hidden";
+setTimeout(()=>{
+  document.body.style.overflow = "auto";
+},3200);
+</script>
